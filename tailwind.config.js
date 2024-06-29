f3d8bff4364976419cf96e4aaff66b6256235b35
Tish <tishthegoat@gmail.com>
2024-06-29 08:51:28 -0600

    all sharper & blog pieces

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/layouts/main.handlebars"],
+  content: [ "./views/**/*.handlebars"],
   theme: {
     extend: {},
   },
